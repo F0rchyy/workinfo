d12bcbd4edc4f4eb530332545af04d96595d4854
F0rchyy <artemovas_work@mail.ru>
2025-06-08 23:04:06 +0300

    Стартовая структура проекта

---
+++ src/test/java/ru/forchy/workinfo/WorkinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.forchy.workinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
