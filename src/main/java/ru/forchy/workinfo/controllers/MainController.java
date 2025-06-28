de384f686f3f8ed452e3b48231020c69b0c83d01
F0rchyy <artemovas_work@mail.ru>
2025-06-28 21:01:34 +0300

    Теперь под админкой можно переходить в редактирование по нажатию кнопки на обычной странице. Редактирование всех страниц работает

--- src/main/java/ru/forchy/workinfo/controllers/MainController.java
+++ src/main/java/ru/forchy/workinfo/controllers/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.forchy.workinfo.entity.Content;
@@ -17,7 +18,7 @@ public class MainController {
     @Autowired
     private ContentRepo contentRepo;
 
-    @GetMapping("/")
+    @GetMapping(value = {"/", "/home"})
     public String home(Model model) {
         model.addAttribute("title", "Главная страница");
 
@@ -31,44 +32,64 @@ public class MainController {
     @GetMapping("/portfolio")
     public String portfolio(Model model) {
         model.addAttribute("title", "Портфолио");
-        return "about";
+
+        Optional<Content> content = contentRepo.findByTag("portfolio");
+        String res = content.map(Content::getContent).orElse(null);
+        model.addAttribute("content", res);
+
+        return "portfolio";
     }
 
     @GetMapping("/about")
     public String about(Model model) {
         model.addAttribute("title", "О себе");
+
+        Optional<Content> content = contentRepo.findByTag("about");
+        String res = content.map(Content::getContent).orElse(null);
+        model.addAttribute("content", res);
+
         return "about";
     }
 
     @GetMapping("/site")
     public String site(Model model) {
         model.addAttribute("title", "Об этом сайте");
-        return "about";
+
+        Optional<Content> content = contentRepo.findByTag("site");
+        String res = content.map(Content::getContent).orElse(null);
+        model.addAttribute("content", res);
+
+        return "site";
     }
 
     @GetMapping("/contact")
     public String contact(Model model) {
         model.addAttribute("title", "Связаться");
-        return "about";
-    }
 
-    @GetMapping("/edit/home")
-    public String editHome(Model model) {
-        model.addAttribute("title", "Главная страница");
-
-        Optional<Content> content = contentRepo.findByTag("home");
+        Optional<Content> content = contentRepo.findByTag("contact");
         String res = content.map(Content::getContent).orElse(null);
         model.addAttribute("content", res);
 
-        return "home-edit";
+        return "contact";
     }
 
-    @PostMapping("/edit/home")
-    public String contentPostUpdate(@RequestParam String text, Model model) {
-        Content content = contentRepo.findByTag("home").orElseThrow();
+    @GetMapping("/edit/{page}")
+    public String editHome(@PathVariable(value = "page") String page, Model model) {
+        model.addAttribute("title", "Редактирование");
+
+        Optional<Content> content = contentRepo.findByTag(page);
+        String res = content.map(Content::getContent).orElse(null);
+        model.addAttribute("content", res);
+
+        return page + "-edit";
+    }
+
+    @PostMapping("/edit/{page}")
+    public String contentPostUpdate(@PathVariable(value = "page") String page, @RequestParam String text, Model model) {
+        Content content = contentRepo.findByTag(page).orElseThrow();
         content.setContent(text);
         contentRepo.save(content);
 
-        return "redirect:/";
+        return "redirect:/" + page;
     }
 }
